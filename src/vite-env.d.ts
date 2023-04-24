c10fa2978787a3214018bc2c9d1429767687a812
Nguyễn Văn Công <congnv3030@gmail.com>
2023-04-24 10:33:23 +0700

    Update type declare for .env variable

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
+/// <reference types="node" />
+/// <reference types="react" />
+/// <reference types="react-dom" />
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+}
