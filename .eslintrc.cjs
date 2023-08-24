e7d7059732178fcde5374e50d43c4d4858d703df
congnv3030 <congnv3030@gmail.com>
2023-08-24 14:00:22 +0700

    update: eslint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -164,7 +164,7 @@ module.exports = {
     ],
     'no-with': 'warn',
     'no-whitespace-before-property': 'warn',
-    'react-hooks/exhaustive-deps': 'off',
+    'react-hooks/exhaustive-deps': 'warn',
     'require-yield': 'warn',
     'rest-spread-spacing': ['warn', 'never'],
     strict: ['warn', 'never'],
