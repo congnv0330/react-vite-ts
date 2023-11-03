2abfe046c081de6a757b25df127a7f765c8efd51
congnv3030 <congnv3030@gmail.com>
2023-11-03 11:02:58 +0700

    chore: add script to build analyze

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,24 @@
 import react from '@vitejs/plugin-react-swc';
 import { fileURLToPath, URL } from 'node:url';
-// import bundleAnalyzer from 'rollup-plugin-bundle-analyzer';
-import { defineConfig, loadEnv } from 'vite';
+import bundleAnalyzer from 'rollup-plugin-bundle-analyzer';
+import { defineConfig, loadEnv, PluginOption } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const _env = loadEnv(mode, process.cwd());
 
+  const plugins: PluginOption[] = [react()];
+
+  if (process.env?.ANALYZE === 'true') {
+    plugins.push(
+      bundleAnalyzer({
+        analyzerMode: 'server',
+      }),
+    );
+  }
+
   return {
-    plugins: [
-      react(),
-      // bundleAnalyzer({
-      //   analyzerMode: 'server',
-      // }),
-    ],
+    plugins,
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url)),
