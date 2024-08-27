873ffc1dd7cc3e630c11ebcff1894867928bf1c8
congnv3030 <congnv3030@gmail.com>
2024-08-27 17:34:45 +0700

    update: add analyze script

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,60 @@
 import react from '@vitejs/plugin-react-swc';
 import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
+import { visualizer } from 'rollup-plugin-visualizer';
+import { createFilter, defineConfig, Plugin, PluginOption } from 'vite';
+
+function removeUseClient(): Plugin {
+  const filter = createFilter(/.*\.(js|ts|jsx|tsx|mjs|cjs)$/);
+
+  return {
+    name: 'remove-use-client',
+
+    transform(code: string, id: string) {
+      if (!filter(id)) {
+        return null;
+      }
+
+      const newCode = code.replace(/['"]use client['"];?\s/g, '');
+
+      return { code: newCode, map: null };
+    },
+  };
+}
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  build: {
-    assetsDir: 'static',
-    rollupOptions: {
-      output: {
-        chunkFileNames: 'static/[hash].chunk.js',
-        assetFileNames: 'static/[hash].[ext]',
+export default defineConfig(({ command }) => {
+  const plugins: PluginOption[] = [react()];
+
+  const isAnalyze: boolean =
+    command === 'build' && process.env.ANALYZE === 'true';
+
+  if (isAnalyze) {
+    plugins.push(
+      removeUseClient(),
+      visualizer({
+        open: true,
+        filename: 'stats.html',
+        sourcemap: true,
+      }),
+    );
+  }
+
+  return {
+    plugins,
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
-  },
+    build: {
+      sourcemap: isAnalyze,
+      assetsDir: 'static',
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'static/[hash].chunk.js',
+          assetFileNames: 'static/[hash].[ext]',
+        },
+      },
+    },
+  };
 });
